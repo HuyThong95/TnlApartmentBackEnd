8ad45471bc68320326cebf5feca7daa1b19ddcba
huythong95 <t.huythonght95@gmail.com>
2019-09-12 08:30:15 +0700

    Create User Repository

---
+++ src/main/java/com/codegym/tnlapartmentbe/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.codegym.tnlapartmentbe.repository;
+
+import com.codegym.tnlapartmentbe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+    Boolean existsByUserName(String userName);
+    Boolean existsByEmail(String email);
+
+}
